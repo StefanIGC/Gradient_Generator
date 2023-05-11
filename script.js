4edb53754a885e6c2427848c8a7d224367c39741
Stefan Chiciuc <stefanchiciuc@gmail.com>
2023-05-11 20:51:08 +0300

    Textarea CSS update

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const gradientBox = document.querySelector(".gradient-box");
 const selectMenu = document.querySelector(".select-box slect");
 const colorInput = document.querySelectorAll(".colors input");
+const textarea = document.querySelectorAll("textarea");
 
 const generateGradient = () => {
     const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`
     gradientBox.style.background = gradient;
+    textarea.value = `background: ${gradient};`;
 }
-
 
 colorInput.forEach(input => {
     input.addEventListener("input", generateGradient);
