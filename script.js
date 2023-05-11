4c20f335e5d97e1bd4ecfe39fec6c7f04e562b4c
Stefan Chiciuc <stefanchiciuc@gmail.com>
2023-05-11 20:47:22 +0300

    Select Box update

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const gradientBox = document.querySelector(".gradient-box");
+const selectMenu = document.querySelector(".select-box slect");
 const colorInput = document.querySelectorAll(".colors input");
 
 const generateGradient = () => {
-    const gradient = `linear-gradient(to top left, ${colorInputs[0].value}, ${colorInputs[1].value})`
+    const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`
     gradientBox.style.background = gradient;
 }
 
 
 colorInput.forEach(input => {
     input.addEventListener("input", generateGradient);
-})
+});
+
+selectMenu.addEventListener("change", generateGradient);
